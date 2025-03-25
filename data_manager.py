4bbedb430d45b2e8670facb46b269a202f810822
= <=>
2025-03-25 20:45:26 -0300

    add get_destination_data method

--- data_manager.py
+++ data_manager.py
@@ -6,10 +6,18 @@ from requests.auth import HTTPBasicAuth
 load_dotenv()
 
 SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/0280f87b4f179fca1b41989e9ec5c2da/flightDeals/prices"
+TOKEN = os.getenv("SHEETY_API_TOKEN")
+
+HEADERS = {"Authorization": f"Bearer {TOKEN}"}
 
 class DataManager:
 
     def __init__(self):
-        self._token = os.environ["SHEETY_TOKEN"]
-        self._authorization = HTTPBasicAuth(self._token)
         self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT, headers=HEADERS)
+        data = response.json()
+        self.destination_data = data["prices"]
+
+        return self.destination_data
