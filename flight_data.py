b698753af6ed0e4dda886dfbc526474ae18229b2
= <=>
2025-03-25 21:40:49 -0300

    flight data for the cheapest flight

---
+++ flight_data.py
@@ -0,0 +1,53 @@
+class FlightData:
+    def __init__(self, price, origin_airport, destination_airport, out_date, return_date):
+        """
+        Constructor for initializing a new flight data instance with specific travel details
+        :param price: the cost of the flight
+        :param origin_airport: the IATA code for the flight's origin airport
+        :param destination_airport: the IATA code for the flight's destination airport.
+        :param out_date: the departure date for the flight.
+        :param return_date: the return date for the flight.
+        """
+        self.price = price
+        self.origin_airport = origin_airport
+        self.destination_airport = origin_airport
+        self.out_date = out_date
+        self.return_date = return_date
+
+def find_cheapest_flight(data):
+    """
+    Parses flight data received from the Amadeus API to identify the cheapest flight option among multiple entries.
+
+    :param data: The JSON data containing flight information returned by the API
+    :return:
+        FlightData: An instance of the FlightData class representing the cheapest flight found,
+        or a FlightData instance where all field are 'NA' if no valid flight data is available.
+    """
+
+    if data is None or not data["data"]:
+        print("No flight data")
+        return FlightData("N/A", "N/A", "N/A", "N/A", "N/A")
+
+    # Data from the first flight in the JSON
+    first_flight = data["data"][0]
+    lowest_price = float(first_flight["price"]["grandTotal"])
+    origin = first_flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+    destination = first_flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
+    out_date  = first_flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
+    return_date  = first_flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
+
+    # Initialize FlightData with the first flight for comparison
+    cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+
+    for flight in data["data"]:
+        price = float(flight["price"]["grandTotal"])
+        if price < lowest_price:
+            lowest_price = price
+            origin = flight["itineraries"][0]["segments"][0]["departure"]["iataCode"]
+            destination  = flight["itineraries"][0]["segments"][0]["arrival"]["iataCode"]
+            out_date  = flight["itineraries"][0]["segments"][0]["departure"]["at"].split("T")[0]
+            return_date  = flight["itineraries"][1]["segments"][0]["departure"]["at"].split("T")[0]
+            cheapest_flight  = FlightData(lowest_price, origin, destination, out_date, return_date)
+            print(f"Lowest price to {destination} is £{lowest_price}")
+
+    return cheapest_flight
