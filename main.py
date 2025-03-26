c9ecaa3af3f5516a71cabe9b6a1ad5302157f187
= <=>
2025-03-26 11:12:21 -0300

    find cheapest flight

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import time
+from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
 flight_search = FlightSearch()
 
+# Set airport origin:
+ORIGIN_CITY_IATA = "CWB"
+
+# Update the airport codes in Google Sheet
 for row in sheet_data:
     if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
+        # Slow down request to avoid rate limit
         time.sleep(2)
 print(f"Sheet Data:\n {sheet_data}")
 
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
+
+# Search for flights
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta(days=(6*30))
+
+for destination in sheet_data:
+    print(f"Getting flight for {destination['city']}...")
+    flights = flight_search.check_flight(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months_from_today
+    )
+    cheapest_flight = find_cheapest_flight(flights)
+    print(f"{destination['city']}: £{cheapest_flight.price}")
+    # Slow down request to avoid rate limit
+    time.sleep(2)
