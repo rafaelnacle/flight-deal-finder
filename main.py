29ca6a806ad4569bb048167ee8fd442094e1b8f6
= <=>
2025-03-25 21:28:59 -0300

    check if the sheet_data contains any values for the iataCode key

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import time
 from data_manager import DataManager
+from flight_search import FlightSearch
+
 data_manager = DataManager()
-
 sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
 
-if sheet_data[0]['iataCode'] == "":
-    from flight_search import FlightSearch
-    flight_search = FlightSearch()
-    for row in sheet_data:
+for row in sheet_data:
+    if row["iataCode"] == "":
         row["iataCode"] = flight_search.get_destination_code(row["city"])
-    print(f"sheet_data:\n {sheet_data}")
+        time.sleep(2)
+print(f"Sheet Data:\n {sheet_data}")
 
-    data_manager.destination_data = sheet_data
-    data_manager.update_destination_codes()
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
